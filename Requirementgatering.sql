ba6589c698febed538481cad1deb103c6472057a
Devarsh <devarsh14@gmail.com>
2018-03-17 09:01:54 +1000

    requirment gathering

---
+++ Requirementgatering.sql
@@ -0,0 +1,28 @@
+-- How can best practices with sql can be co-operated with dotnet core 
+-- Main Aim here is to Create an applicaiton which helps me to Understand concepst s
+-- Ticketing system for cleaning services 
+-- Monitor live issues 
+-- if same complain from same area comes again and again than it becomes issue and supervisor need to visit a system during cleanign working hour -- and describe the cleaner whats going wrong 
+-- stock utitility counting from clearn perspective 
+-- restock cleaning material once coming to complete 
+-- In long run predicte how much stock user consume on site with services 
+
+-- Provide cleaner a feed back 
+-- cleanin score 
+-- Proactive cleanign 
+-- divide building in areas 
+-- provide more attention to the area if same kind of complain is repetitive 
+-- define complain nature 
+-- How can we define and quantify cleaners efforrt ...
+
+-- Automatic resolution send email facility to client 
+-- provide real time tracking of issue 
+
+-- priority of issue 
+-- severity of issue 
+
+
+-- How organisation can help cleaner to understand issue 
+
+
+
