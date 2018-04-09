09d7b03b323c47738ac77cd45aa12b580b545fe1
Devarsh <devarsh14@gmail.com>
2018-04-10 06:32:03 +1000

    regular commands added

---
+++ cleaningsoftWebApp/Models/CleaningServiceType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cleaningsoftWebApp.Models
+{
+    public partial class CleaningServiceType
+    {
+        public Guid Id { get; set; }
+        public string ServiceTypeName { get; set; }
+    }
+}
