1d529af80d03fa82f08aec1d4e349fc167d49c1c
Devarsh <devarsh14@gmail.com>
2018-04-10 06:35:03 +1000

    new added

---
+++ RegularCommands.sql
@@ -0,0 +1,10 @@
+/*
+dotnet ef dbcontext scaffold "Server=localhost;Database=CleaningSoftwareLogic;User ID=dev2;Password=Dcs1989.." Microsoft.EntityFrameworkCore.SqlServer -o Models -f
+
+1. Next time instead of building all models again tryi to change a connection string and test 
+
+2. any error delete all models 
+-NOTE Modles pluralise
+
+MVC reason is it provides two kind of function API and VIEWS basic app will need some kind of view to run
+*/
