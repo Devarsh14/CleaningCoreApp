715a2d322dfe4fa4786a52516a2c00f6987aaa83
Devarsh14 <Devarsh14@gmail.com>
2018-04-29 22:24:40 +1000

    git commit added comment and view logic.

--- cleaningsoftWebApp/Controllers/CleaningServiceProviderCRUDController.cs
+++ cleaningsoftWebApp/Controllers/CleaningServiceProviderCRUDController.cs
@@ -23,17 +23,27 @@ namespace cleaningsoftWebApp.Controllers
 
             return View(cleaningServiceProvidersList);
         }
+       // [HttpPost] Not at this stage -- In ajax will use only as a Post
         public IActionResult Add (CleaningServiceProviders cleaningServiceProviders)
         {
-            CleaningServiceProviders cleaningServiceProviders2 = new CleaningServiceProviders();
-            cleaningServiceProviders2 = cleaningServiceProviders;
+            // If model is empty or first attept made to go to add page it will go to view of add
+            // If model is not empty means information has been added on to the add page it will execute add command and redirects to list or indiex page
+            // Need to add a add button which redirecto to this action and view on to the List page so user can have a naviation path from list page for adding a new company otherwise have to redirect manually. 
+            if (cleaningServiceProviders.CompanyName==null)
+            {
+                return View();
+            }
+            else
+            {
+
+                CleaningServiceProviders cleaningServiceProviders2 = new CleaningServiceProviders();
+                cleaningServiceProviders2 = cleaningServiceProviders;
+                this.cleaningSoftwareLogicContext.Add(cleaningServiceProviders2);
+                this.cleaningSoftwareLogicContext.SaveChanges();
+
+                return RedirectToAction("Index", "CleaningServiceProviderCRUD");
+            }
             
-
-
-            this.cleaningSoftwareLogicContext.Add(cleaningServiceProviders2);
-            this.cleaningSoftwareLogicContext.SaveChanges();
-
-            return View();
         }
 
     }
