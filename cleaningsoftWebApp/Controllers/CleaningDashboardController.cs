4c9b3eb63ed52c164bd58c64fc5393f3e563db6d
Devarsh14 <Devarsh14@gmail.com>
2018-04-29 19:51:38 +1000

    Cleanign features.

--- cleaningsoftWebApp/Controllers/CleaningDashboardController.cs
+++ cleaningsoftWebApp/Controllers/CleaningDashboardController.cs
@@ -1,7 +1,19 @@
 namespace cleaningsoftWebApp.Controllers
 {
-    public class CleaningDashboardController
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using cleaningsoftWebApp.Models;
+
+
+    public class CleaningDashboardController : Controller
     {
-        
+        //public IActionResult ServiceTypeApi()
+        //{
+            
+        //}
     }
 }
