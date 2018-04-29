1d369e875afde2294177ca5ace8b56839cbd2712
Devarsh14 <Devarsh14@gmail.com>
2018-04-29 22:09:19 +1000

    New Add entity into database working fine with crude.

---
+++ cleaningsoftWebApp/Controllers/CleaningServiceProviderCRUDController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cleaningsoftWebApp.Models;
+
+namespace cleaningsoftWebApp.Controllers
+{
+    
+    public class CleaningServiceProviderCRUDController : Controller
+    {
+        private readonly CleaningSoftwareLogicContext cleaningSoftwareLogicContext;
+        public CleaningServiceProviderCRUDController(CleaningSoftwareLogicContext cleaningSoftwareLogicContext)
+        {
+            this.cleaningSoftwareLogicContext = cleaningSoftwareLogicContext;
+        }
+        
+        // Cleanign service providers controller home page
+        public IActionResult Index()
+        {
+            IList<CleaningServiceProviders> cleaningServiceProvidersList = this.cleaningSoftwareLogicContext.CleaningServiceProviders.ToList();
+
+            return View(cleaningServiceProvidersList);
+        }
+        public IActionResult Add (CleaningServiceProviders cleaningServiceProviders)
+        {
+            CleaningServiceProviders cleaningServiceProviders2 = new CleaningServiceProviders();
+            cleaningServiceProviders2 = cleaningServiceProviders;
+            
+
+
+            this.cleaningSoftwareLogicContext.Add(cleaningServiceProviders2);
+            this.cleaningSoftwareLogicContext.SaveChanges();
+
+            return View();
+        }
+
+    }
+}
--- cleaningsoftWebApp/Startup.cs
+++ cleaningsoftWebApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using cleaningsoftWebApp.Models;
 
 namespace cleaningsoftWebApp
 {
@@ -22,6 +23,7 @@ namespace cleaningsoftWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<CleaningSoftwareLogicContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
